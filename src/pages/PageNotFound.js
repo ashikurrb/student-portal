1c6e048c44f50b86c959cdb4ce6dac5edd6e8411
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-06-06 18:09:34 +0600

    Go Back Button Updated

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layouts/Layout';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const PageNotFound = () => {
+    const navigate = useNavigate();
 
+    const handleGoBack = () => {
+        navigate(-1);
+    };
     return (
         <Layout title={"Page Not Found"}>
             <div className="pnf">
                 <div className="pnf-title">404</div>
-                <h2 className="pnf-heading"> Oops! Page Not Found</h2>
-                <Link to={"/"} className="pnf-btn"><b>Go Back</b></Link>
+                <h2 className="pnf-heading"> Ooops! Page Not Found</h2>
+                <button className='pnf-btn' onClick={handleGoBack}><b>Go Back</b></button>
+                <Link to="/" className="pnf-btn"><b>Go to Home</b></Link>
             </div>
         </Layout>
     );
