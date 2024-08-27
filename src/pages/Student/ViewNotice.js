7be92fc3087c922e2e7c86e47ce9b0ca890c6372
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-08-28 03:12:37 +0600

    view update notice

--- src/pages/Student/ViewNotice.js
+++ src/pages/Student/ViewNotice.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 const ViewNotice = () => {
     const [notice, setNotice] = useState([]);
     const [spinnerLoading, setSpinnerLoading] = useState(true);
+    const [selectedImage, setSelectedImage] = useState(null); // State to hold the selected image
 
     const getContent = async () => {
         try {
@@ -18,41 +19,79 @@ const ViewNotice = () => {
             setSpinnerLoading(false);
         }
     };
+
     useEffect(() => {
         getContent();
     }, []);
+
+    // Function to handle image click
+    const handleImageClick = (img) => {
+        setSelectedImage(img);
+    };
+
+    // Function to close the modal
+    const handleCloseModal = () => {
+        setSelectedImage(null);
+    };
+
     return (
         <Layout title={"Notice"}>
             <div className="container-fluid mt-3 p-3">
                 <div className="row">
-                   <div className="col-md-12">
-                   <h2 className='text-center my-3'>Notice</h2>
-                   {
-                    spinnerLoading ? <div className="text-center m-5">
-                        <Spinner /> <p>Loading Notice...</p>
-                    </div> : <div className="row">
+                    <div className="col-md-12">
+                        <h2 className='text-center my-3'>Notice</h2>
                         {
-                            notice.map((n, i) => {
-                                return (
-                                    <div className="card m-2 d-lg-flex">
-                                       <div className="col-md-6">
-                                       <h5>{n.title}</h5>
-                                        <span>Created: {moment(n.createdAt).fromNow()}</span>
-                                        <span>Grade: {n?.grade?.name}</span>
-                                        <p>{n.noticeInfo}</p>
-                                       </div>
-                                       <div className="col-md-6">
-                                       <span><img style={{width:"50px"}} src={n.noticeImg} alt="" /></span>
-                                       </div>
-                                    </div>
-                                )
-                            })
+                            spinnerLoading ? <div className="text-center m-5">
+                                <Spinner /> <p>Loading Notice...</p>
+                            </div> : <div className="row">
+                                {
+                                    notice.map((n, i) => {
+                                        return (
+                                            <div className="card me-5 my-2 ps-4 py-3" key={i}>
+                                                <div className="d-flex">
+                                                    <div className="col-md-6">
+                                                        <figcaption className='blockquote-footer'>{moment(n.createdAt).fromNow()}</figcaption>
+                                                        <h5>{n.title}</h5>
+                                                        <span>Grade: {n?.grade?.name}</span>
+                                                        <p>{n.noticeInfo}</p>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className='mx-5 my-22'>
+                                                            <img 
+                                                                style={{ width: "150px", cursor: "pointer" }} 
+                                                                src={n.noticeImg} 
+                                                                alt="" 
+                                                                onClick={() => handleImageClick(n.noticeImg)} 
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
                         }
                     </div>
-                }
-                   </div>
                 </div>
             </div>
+
+            {/* Modal */}
+            {selectedImage && (
+                <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content shadow border border-3">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Image Preview</h5>
+                                <button type="button" className="btn-close" aria-label="Close" onClick={handleCloseModal}></button>
+                            </div>
+                            <div className="modal-body">
+                                <img src={selectedImage} alt="Notice" style={{ width: '100%' }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </Layout>
     );
 };
