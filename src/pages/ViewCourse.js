d842a9e9bd1093b0ca63cbbf3521c4a6c685d28d
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-09-02 01:46:40 +0600

    update ui

--- src/pages/ViewCourse.js
+++ src/pages/ViewCourse.js
@@ -5,7 +5,7 @@ const ViewCourse = () => {
     return (
         <Layout>
         <div className="container">
-            <div className="row">
+            <div className="row p-3">
             <h3 className='text-center my-3'>Available Course</h3>
             <div className="card text-center h2 p-5 mt-5 text-secondary ">No course available yet</div>
             </div>
